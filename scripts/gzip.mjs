27e56e69aca80f0b562f620c898942b112a9ea51
CRIMX <straybugs@gmail.com>
2023-05-17 22:59:26 +0800

    chore: print min mjs bundle size

--- scripts/gzip.mjs
+++ scripts/gzip.mjs
@@ -4,6 +4,11 @@ import prettyBytes from "pretty-bytes";
 
 console.log();
 console.log(
+  `${gray("gzip")} dist/index.mjs ${green(
+    prettyBytes(gzipSizeFromFileSync("dist/index.mjs"))
+  )}`
+);
+console.log(
   `${gray("gzip")} dist/index.js  ${green(
     prettyBytes(gzipSizeFromFileSync("dist/index.js"))
   )}`
